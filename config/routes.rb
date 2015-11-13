5a6d37059d807ef0df92ee6782a69cb38afbdffa
charissatolentino@gmail.com <hitobochi@Risa-Tolentinos-MacBook-Pro.local>
2015-11-13 09:54:44 -0600

    added a new message controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'messages/hello'
+
   resources :students
   resources :courses
   # The priority is based upon order of creation: first created -> highest priority.
